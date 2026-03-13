2d3f18c1b7f411c1f2e52a6697a5302f036e821f
jtizzler <jtischermba@gmail.com>
2026-03-13 14:15:01 -0400

    Update streamlit_app.py
    fix session = get connection issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title(":cup_with_straw:Customize Your Smoothie :cup_with_straw:")
+st.title("Customize Your Smoothie :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your custom Smoothie.
   """
@@ -16,7 +16,9 @@ st.write(
 # )
 # st.write('You selected:', option)
 
-session = get_active_session()
+# session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
